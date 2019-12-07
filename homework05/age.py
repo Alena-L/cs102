0e40a2c3cd354f8944d5f2aeb93da7d92eda047c
Alena-L <alena.lutskovich@gmail.com>
2019-12-07 22:56:48 +0300

    Release 1.0

--- homework05/age.py
+++ homework05/age.py
@@ -3,7 +3,7 @@ from statistics import median
 from typing import Optional
 
 from api import get_friends
-from api_models import User
+
 
 
 def age_predict(user_id: int) -> Optional[float]:
@@ -14,4 +14,26 @@ def age_predict(user_id: int) -> Optional[float]:
     """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    friends = get_friends(user_id, 'bdate')
+    if friends is not None:
+        ages = []
+        for i in range(len(friends)):
+            try:
+                birthday = friends[i]['bdate']
+            except KeyError:
+                pass
+            else:
+                fulldate = birthday.split('.')
+                if len(fulldate) == 3:
+                    day = dt.datetime.now().day
+                    month = dt.datetime.now().month
+                    year = dt.datetime.now().year
+                    if month > int(fulldate[1]) or month == int(fulldate[1]) and day >= int(fulldate[0]):
+                        ages.append(year - int(fulldate[2]))
+                    else:
+                        ages.append(year - int(fulldate[2]) - 1)
+        if len(ages) == 0:
+            return None
+        else:
+            return median(ages)
+    return None
