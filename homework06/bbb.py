2e220848bbd48a4c998315184b3c8cab06583993
Alena-L <55996738+Alena-L@users.noreply.github.com>
2020-02-29 23:26:46 +0300

    Delete bbb.py

--- homework06/bbb.py
+++
@@ -1,8 +0,0 @@
-from bottle import route, run, template
-
-@route('/')
-@route('/hello/<name>')
-def index(name="Pretty :3"):
-    return template('<em>Hello {{name}}</em>!', name=name)
-
-run(host='localhost', port=8080)
